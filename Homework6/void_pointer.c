489fd8fa12d618d3e5dd25b2433c224ba4fe34ae
Milen <milenchavdarov@gmail.com>
2022-06-12 12:59:30 +0300

    Homework6 - Task3

---
+++ Homework6/void_pointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define TINT int
+#define TCHAR char
+#define TFLOAT float
+#define TDOUBLE double
+#define TUINT8 uint8_t
+#define TUINT16 uint16_t
+#define TUINT32 uint32_t
+#define TUINT64 uint64_t
+
+void printValue(const void *valuePtr, uint8_t flag);
+
+int main()
+{
+
+    return EXIT_SUCCESS;
+}
