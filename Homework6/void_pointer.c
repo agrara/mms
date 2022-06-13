728b66a280c15c66d6e690523b195248480e2cf7
Milen <milenchavdarov@gmail.com>
2022-06-13 06:41:42 +0300

    Homework6 - Task3

--- Homework6/void_pointer.c
+++ Homework6/void_pointer.c
@@ -2,19 +2,63 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define TINT int
-#define TCHAR char
-#define TFLOAT float
-#define TDOUBLE double
-#define TUINT8 uint8_t
-#define TUINT16 uint16_t
-#define TUINT32 uint32_t
-#define TUINT64 uint64_t
+#define TINT 1
+#define TCHAR 2
+#define TFLOAT 3
+#define TDOUBLE 4
+#define TUINT8 5
+#define TUINT16 6
+#define TUINT32 7
+#define TUINT64 8
 
 void printValue(const void *valuePtr, uint8_t flag);
 
 int main()
 {
+    int n = 23;
+    double num = 3.14;
+    char symbol = 'A';
+
+    printValue(&n, TINT);
+    printValue(&num, TDOUBLE);
+    printValue(&symbol, TCHAR);
 
     return EXIT_SUCCESS;
+}
+
+void printValue(const void *valuePtr, uint8_t flag)
+{
+    if (flag == 1)
+    {
+        printf("%d\n", *((int *)valuePtr));
+    }
+    else if (flag == 2)
+    {
+        printf("%c\n", *((char *)valuePtr));
+    }
+    else if (flag == 3)
+    {
+        printf("%.2f\n", *((float *)valuePtr));
+    }
+    else if (flag == 4)
+    {
+        printf("%.2lf\n", *((double *)valuePtr));
+    }
+    else if (flag == 5)
+    {
+        printf("%d\n", *((uint8_t *)valuePtr));
+    }
+
+    else if (flag == 6)
+    {
+        printf("%d\n", *((uint16_t *)valuePtr));
+    }
+    else if (flag == 7)
+    {
+        printf("%d\n", *((uint32_t *)valuePtr));
+    }
+    else if (flag == 9)
+    {
+        printf("%ld\n", *((uint64_t *)valuePtr));
+    }
 }
