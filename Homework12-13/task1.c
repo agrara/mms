acb046f5f8658aef4116f8849d2b9d58616a8904
Milen <milenchavdarov@gmail.com>
2022-06-18 17:01:09 +0300

    Homework12-13 - Task1

---
+++ Homework12-13/task1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+unsigned char CheckBit(unsigned int uValue);
+
+int main()
+{
+    printf("%d\n", CheckBit(2));
+}
+
+unsigned char CheckBit(unsigned int uValue)
+{
+    int flag = 0;
+    for (int i = 0; i < sizeof(uValue) * 8; i++)
+    {
+        if ((uValue & (1 << i)) && flag == 0)
+        {
+            flag = 1;
+        }
+        else if ((uValue & (1 << i)) && flag == 1)
+        {
+            return 0;
+        }
+    }
+    return flag;
+}
